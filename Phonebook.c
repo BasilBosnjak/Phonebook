77a9b1fd66917ecbe1d861389328d1af371a0645
BasilBosnjak <basil.bosnjak@gmail.com>
2021-08-11 20:43:01 +0200

    Inputs

--- Phonebook.c
+++ Phonebook.c
@@ -52,7 +52,55 @@ switch(getch())
                 addrecord();
     break;
    case '2': listrecord();
-
-
+    break;
+    case '3': exit(0);
+    break;
+    case '5': searchrecord();
+    break;
+    case '6': deleterecord();
+    break;
+    default:
+                system("cls");
+                printf("\nEnter 1 to 6 only");
+                printf("\n Enter any key");
+                getch();
+menu();
 }
+}
+        void addrecord()
+{
+        system("cls");
+        FILE *f;
+        struct person p;
+        f=fopen("project","ab+");
+        printf("\n Enter name: ");
+        got(p.name);
+        printf("\nEnter the address: ");
+        got(p.address);
+        printf("\nEnter father name: ");
+        got(p.father_name);
+        printf("\nEnter mother name: ");
+        got(p.mother_name);
+        printf("\nEnter phone no.:");
+        scanf("%ld",&p.mble_no);
+        printf("Enter sex:");
+        got(p.sex);
+        printf("\nEnter e-mail:");
+         got(p.mail);
+        printf("\nEnter citizen no:");
+        got(p.citision_no);
+        fwrite(&p,sizeof(p),1,f);
+
+      fflush(stdin);
+        printf("\nrecord saved");
+
+fclose(f);
+
+    printf("\n\nEnter any key");
+
+	 getch();
+    system("cls");
+    menu();
+
+
 }
