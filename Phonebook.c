c55d6da83bc885bc442638fe5d4f9934f95ffcdd
BasilBosnjak <basil.bosnjak@gmail.com>
2021-08-12 14:25:29 +0200

    Options

--- Phonebook.c
+++ Phonebook.c
@@ -101,6 +101,97 @@ fclose(f);
 	 getch();
     system("cls");
     menu();
+}
+void listrecord()
+{
+    struct person p;
+    FILE *f;
+f=fopen("project","rb");
+if(f==NULL)
+{
+printf("\nfile opening error in listing :");
 
+exit(1);
+}
+while (fread(&p,sizeof(p),1,f)==1)
+{
+     printf("\n\n\n YOUR RECORD IS\n\n ");
+        printf("\nName=%s\nAdress=%s\nFather name=%s\nMother name=%s\nMobile no=%ld\nSex=%s\nE-mail=%s\nCitizen no=%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
+
+	 getch();
+	 system("cls");
+}
+fclose(f);
+ printf("\n Enter any key");
+ getch();
+    system("cls");
+menu();
+}
+void searchrecord()
+{
+    struct person p;
+FILE *f;
+char name[100];
+
+f=fopen("project","rb");
+if(f==NULL)
+{
+    printf("\n error in opening\a\a\a\a");
+    exit(1);
 
 }
+printf("\nEnter name of person to search\n");
+got(name);
+while(fread(&p,sizeof(p),1,f)==1)
+{
+    if(strcmp(p.name,name)==0)
+    {
+        printf("\n\tDetail Information About %s",name);
+        printf("\nName:%s\naddress:%s\nFather name:%s\nMother name:%s\nMobile no:%ld\nsex:%s\nE-mail:%s\nCitision no:%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
+    }
+        else
+        printf("file not found");
+
+}
+ fclose(f);
+  printf("\n Enter any key");
+
+	 getch();
+    system("cls");
+menu();
+}
+void deleterecord()
+{
+    struct person p;
+    FILE *f,*ft;
+	int flag;
+	char name[100];
+	f=fopen("project","rb");
+	if(f==NULL)
+		{
+
+			printf("CONTACT'S DATA NOT ADDED YET.");
+
+		}
+	else
+	{
+		ft=fopen("temp","wb+");
+		if(ft==NULL)
+
+            printf("file opaning error");
+		else
+
+        {
+
+
+		printf("Enter CONTACT'S NAME:");
+		got(name);
+
+		fflush(stdin);
+		while(fread(&p,sizeof(p),1,f)==1)
+		{
+			if(strcmp(p.name,name)!=0)
+				fwrite(&p,sizeof(p),1,ft);
+			if(strcmp(p.name,name)==0)
+                flag=1;
+		}
