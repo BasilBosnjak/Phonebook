011204585453a0ade46c04aed05e3304812e00d6
BasilBosnjak <basil.bosnjak@gmail.com>
2021-08-13 11:23:32 +0200

    Finished

--- Phonebook.c
+++ Phonebook.c
@@ -195,3 +195,134 @@ void deleterecord()
 			if(strcmp(p.name,name)==0)
                 flag=1;
 		}
+    fclose(f);
+	fclose(ft);
+	if(flag!=1)
+	{
+		printf("NO CONACT'S RECORD TO DELETE.");
+		remove("temp.txt");
+	}
+		else
+		{
+			remove("project");
+			rename("temp.txt","project");
+			printf("RECORD DELETED SUCCESSFULLY.");
+
+		}
+	}
+}
+ printf("\n Enter any key");
+
+	 getch();
+    system("cls");
+menu();
+}
+
+void modifyrecord()
+{
+    int c;
+    FILE *f;
+    int flag=0;
+    struct person p,s;
+	char  name[50];
+	f=fopen("project","rb+");
+	if(f==NULL)
+		{
+
+			printf("CONTACT'S DATA NOT ADDED YET.");
+			exit(1);
+
+
+		}
+	else
+	{
+	    system("cls");
+		printf("\nEnter CONTACT'S NAME TO MODIFY:\n");
+            got(name);
+            while(fread(&p,sizeof(p),1,f)==1)
+            {
+                if(strcmp(name,p.name)==0)
+                {
+
+
+
+                    printf("\n Enter name:");
+                    got(s.name);
+                    printf("\nEnter the address:");
+                    got(s.address);
+                    printf("\nEnter father name:");
+                    got(s.father_name);
+                    printf("\nEnter mother name:");
+                    got(s.mother_name);
+                    printf("\nEnter phone no:");
+                    scanf("%ld",&s.mble_no);
+                    printf("\nEnter sex:");
+                    got(s.sex);
+                    printf("\nEnter e-mail:");
+                    got(s.mail);
+                    printf("\nEnter citizen no\n");
+                    got(s.citision_no);
+                    fseek(f,-sizeof(p),SEEK_CUR);
+                    fwrite(&s,sizeof(p),1,f);
+                    flag=1;
+                    break;
+
+
+
+                }
+                fflush(stdin);
+
+
+            }
+            if(flag==1)
+            {
+                printf("\n your data id modified");
+
+            }
+            else
+            {
+                    printf(" \n data is not found");
+
+            }
+            fclose(f);
+	}
+	 printf("\n Enter any key");
+	 getch();
+    system("cls");
+	menu();
+
+}
+void got(char *name)
+{
+
+   int i=0,j;
+    char c,ch;
+    do
+    {
+        c=getch();
+                if(c!=8&&c!=13)
+                {
+                    *(name+i)=c;
+                        putch(c);
+                        i++;
+                }
+                if(c==8)
+                {
+                    if(i>0)
+                    {
+                        i--;
+                    }
+                   // printf("h");
+                    system("cls");
+                    for(j=0;j<i;j++)
+                    {
+                        ch=*(name+j);
+                        putch(ch);
+
+                    }
+
+                }
+    }while(c!=13);
+      *(name+i)='\0';
+}
+       
